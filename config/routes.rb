3e6bc3c626c3b6deaab08919e24ac26fd3289781
Keith Johnson <kjohnson0451@gmail.com>
2017-03-14 12:57:34 -0400

    Added forward slashes to the beginning of the paths for good measure

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # WEEKS RESOURCES
   get 'weeks', to: 'weeks#index'
-  post 'create_week', to: 'weeks#create', path: 'weeks/create'
-  delete 'week', to: 'weeks#destroy', path: 'weeks/:id'
+  post 'create_week', to: 'weeks#create', path: '/weeks/create'
+  delete 'week', to: 'weeks#destroy', path: '/weeks/:id'
 
 end
