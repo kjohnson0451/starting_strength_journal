156c01b1c65d81a81f613867b758586c14c317c2
Keith Johnson <kjohnson0451@gmail.com>
2017-03-21 15:12:21 -0400

    Removed get exercise route. It didn't seem necessary. Also cleaned up the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
   # WEEKS RESOURCES
-  get '/weeks/', to: 'weeks#index', as: 'weeks', action: 'weeks'
-  post '/weeks/create', to: 'weeks#create', as: 'create_week', action: 'create_week'
-  delete '/weeks/:id', to: 'weeks#destroy', as: 'week', action: 'week'
+  get '/weeks/', to: 'weeks#index', as: 'weeks'
+  post '/weeks/create', to: 'weeks#create', as: 'create_week'
+  delete '/weeks/:id', to: 'weeks#destroy', as: 'week'
 
   # EXERCISES RESOURCES
-  get '/exercises/:id', to: 'exercises#show', as: 'exercise', action: 'exercise'
-  patch '/exercises/:id', to: 'exercises#update', as: '', action: ''
-  put '/exercises/:id', to: 'exercises#update', as: '', action: ''
+  patch '/exercises/:id', to: 'exercises#update', as: 'exercise'
+  put '/exercises/:id', to: 'exercises#update'
 
 end
