2219d2f1bff6f7cd9937a28e385a6c5d621ad32c
Keith Johnson <kjohnson0451@gmail.com>
2017-03-31 15:37:49 -0400

    Seeds the DB with a new week.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Routine.create()
+routine = Routine.create()
+
+week = Week.create(position: 1, routine: routine)
+
+days = Day.create([{week: week, name: "Monday"}, {week: week, name: "Wednesday"}, {week: week, name: "Friday"}])
+
+Exercise.create(day: days[0], position: 1, description: "3x5 Squat")
+Exercise.create(day: days[0], position: 2, description: "3x5 Press")
+Exercise.create(day: days[0], position: 3, description: "1x5 Deadlift")
+
+Exercise.create(day: days[1], position: 1, description: "3x5 Squat")
+Exercise.create(day: days[1], position: 2, description: "3x5 Bench")
+Exercise.create(day: days[1], position: 3, description: "1x5 Deadlift")
+
+Exercise.create(day: days[2], position: 1, description: "3x5 Squat")
+Exercise.create(day: days[2], position: 2, description: "3x5 Press")
+Exercise.create(day: days[2], position: 3, description: "1x5 Deadlift")
